f6d42c9dd6cb7c79fec2c2721fc20c8c4bc76b1f
q8888620002 <q8888620002@users.noreply.github.com>
2017-02-13 21:25:36 +0800

    Update library.js

--- library.js
+++ library.js
@@ -20,7 +20,12 @@ var db = module.parent.require('./database'),
 			return '\\' + match;
 		});
 	},
-
+	
+	client = new elasticsearch.Client({
+  		host: 'localhost:9200'
+  		// log: 'trace'
+	}),
+	// this config dosen't work for newer version of elasticsearch api 
 	Elasticsearch = {
 		/*
 			Defaults configs:
@@ -241,13 +246,14 @@ Elasticsearch.search = function(data, callback) {
 			size: 20
 		}
 	};
-
-	Elasticsearch.client.search(query, function(err, obj) {
+	// changing the client obj
+	client.search(query, function(err, obj) {
 		if (err) {
 			callback(err);
 		} else if (obj && obj.hits && obj.hits.hits && obj.hits.hits.length > 0) {
 			var payload = obj.hits.hits.map(function(result) {
-				return parseInt(result._id, 10);
+				// return the correct post id 
+				return parseInt(result._source.pid, 10);
 			});
 
 			callback(null, payload);
